dfde31ea8929a6f7d0464916494cf3c363322960
Jeff Winchell <jmw479@drexel.edu>
2019-04-20 20:45:41 -0400

    aruco marker detection

--- trackMarkers/trackMarkers.py
+++ trackMarkers/trackMarkers.py
@@ -1,98 +1,60 @@
-from pyimagesearch.centroidtracker import CentroidTracker
-from pyimagesearch.trackableobject import TrackableObject
-from imutils.video import VideoStream
-from imutils.video import FPS
-import numpy as np
-import argparse
-import imutils
-import time
-import dlib
+# The following code is used to watch a video stream, detect Aruco markers, and use
+# a set of markers to determine the posture of the camera in relation to the plane
+# of markers.
+#
+# Assumes that all markers are on the same plane, for example on the same piece of paper
+#
+# Requires camera calibration (see the rest of the project for example calibration)
+
+import numpy
 import cv2
+import cv2.aruco as aruco
 
-# construct the argument parser and parse the arguments
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
-ap.add_argument("-i", "--input", type=str, help="path to optional input video file")
-ap.add_argument("-o", "--output", type=str, help="path to optional output video file")
-ap.add_argument("-c", "--confidence", type=float, default=0.4, help="minimum probability to filter weak detections")
-ap.add_argument("-s", "--skip-frames", type=int, default=30, help="# of skip frames between detections")
-args = vars(ap.parse_args())
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_4X4_1000)
 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+# Create grid board object we're using in our stream
+board = aruco.GridBoard_create(
+    markersX=2,
+    markersY=2,
+    markerLength=0.09,
+    markerSeparation=0.01,
+    dictionary=ARUCO_DICT)
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# Create vectors we'll be using for rotations and translations for postures
+rvecs, tvecs = None, None
 
-# if a video path was not supplied, grab a reference to the webcam
-if not args.get("input", False):
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(2.0)
+cam = cv2.VideoCapture(0)
 
-# otherwise, grab a reference to the video file
-else:
-	print("[INFO] opening video file...")
-	vs = cv2.VideoCapture(args["input"])
+while (cam.isOpened()):
+    # Capturing each frame of our video stream
+    ret, QueryImg = cam.read()
+    if ret == True:
+        # grayscale image
+        gray = cv2.cvtColor(QueryImg, cv2.COLOR_BGR2GRAY)
 
-# initialize the video writer (we'll instantiate later if need be)
-writer = None
+        # Detect Aruco markers
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
 
-# initialize the frame dimensions (we'll set them as soon as we read
-# the first frame from the video)
-W = None
-H = None
+        # Make sure all 5 markers were detected before printing them out
+        if ids is not None:
+            # Print corners and ids to the console
+            for i, corner in zip(ids, corners):
+                print('ID: {}; Corners: {}'.format(i, corner))
 
-# instantiate our centroid tracker, then initialize a list to store
-# each of our dlib correlation trackers, followed by a dictionary to
-# map each unique object ID to a TrackableObject
-ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
-trackers = []
-trackableObjects = {}
+            # Outline all of the markers detected in our image
+            QueryImg = aruco.drawDetectedMarkers(QueryImg, corners, borderColor=(0, 0, 255))
 
-# initialize the total number of frames processed thus far, along
-# with the total number of objects that have moved either up or down
-totalFrames = 0
-totalDown = 0
-totalUp = 0
+            # Wait on this frame
+            # if cv2.waitKey(0) & 0xFF == ord('q'):
+            #     break
 
-# start the frames per second throughput estimator
-fps = FPS().start()
+        # Display our image
+        cv2.imshow('QueryImage', QueryImg)
 
-class TrackableObject:
-	def __init__(self, objectID, centroid):
-		# store the object ID, then initialize a list of centroids
-		# using the current centroid
-		self.objectID = objectID
-		self.centroids = [centroid]
+    # Exit at the end of the video on the 'q' keypress
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-		# initialize a boolean used to indicate if the object has
-		# already been counted or not
-		self.counted = False
-
-OPENCV_OBJECT_TRACKERS = {
-	"csrt": cv2.TrackerCSRT_create,
-	"kcf": cv2.TrackerKCF_create,
-	"boosting": cv2.TrackerBoosting_create,
-	"mil": cv2.TrackerMIL_create,
-	"tld": cv2.TrackerTLD_create,
-	"medianflow": cv2.TrackerMedianFlow_create,
-	"mosse": cv2.TrackerMOSSE_create
-}
-# initialize OpenCV's special multi-object tracker
-trackers = cv2.MultiTracker_create()
-
-cap = cv2.VideoCapture(0)
-
-time.sleep(2.0)
-
-
-cap.release()
 cv2.destroyAllWindows()
