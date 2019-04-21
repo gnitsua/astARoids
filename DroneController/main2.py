3bb5f959ae85c18d9d6fccc488e59902c025c478
Jeff Winchell <jmw479@drexel.edu>
2019-04-21 05:02:16 -0400

    drone controller

--- DroneController/main2.py
+++ DroneController/main2.py
@@ -1,5 +1,6 @@
 import cv2
 from pyardrone.video import VideoClient
+import png
 
 import logging
 
@@ -9,10 +10,16 @@ logging.basicConfig(level=logging.DEBUG)
 client = VideoClient('192.168.1.1', 5555)
 client.connect()
 client.video_ready.wait()
+i = 0
 try:
     while True:
         cv2.imshow('im', client.frame)
-        if cv2.waitKey(10) == ord(' '):
+        if cv2.waitKey(10) == ord('s'):
+            print(client.frame)
+            # png.from_array(client.frame, 'L').save("frame{}.png".format(i))
+            cv2.imwrite('frame{}.png'.format(i), client.frame)
+            i = i + 1
+        elif cv2.waitKey(10) == ord(' '):
             break
 finally:
     client.close()
