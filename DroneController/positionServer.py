a378d7a1b40f4415726735828b50d12ae0bb5f75
mudkipmaster <acg59@drexel.edu>
2019-04-21 06:46:53 -0400

    updated positionServer

--- DroneController/positionServer.py
+++ DroneController/positionServer.py
@@ -1,6 +1,6 @@
 import time
 from random import randint
-
+import logging
 import cv2
 import zmq as zmq
 from pyardrone.video import VideoClient
@@ -18,19 +18,36 @@ class PositionServer():
 
 
 if __name__ == "__main__":
+
+
+
+    logging.basicConfig(level=logging.DEBUG)
     ps = PositionServer("5556")
-    tracker = ArucoCornerTracker('webcam.npz')
+    tracker = ArucoCornerTracker('drone.npz')
     client = VideoClient('192.168.1.1', 5555)
     client.connect()
     client.video_ready.wait()
     i = 0
     try:
         while True:
-            cv2.imshow('im', client.frame)
-            tvec = tracker.getCameraPosition(client.frame)
-            print(
-                "x: %.2f cm, y: %.2f cm, z: %.2f cm, Pitch is %.2f degrees, Yaw is %.2f degrees, Roll is %.2f degrees." % (
-                    tvec[0] / 10.0, tvec[1] / 10.0, tvec[2] / 10.0, tvec[3], tvec[4], tvec[5]))
-            # ps.sendPosition(x, y, z)
+
+            try:
+                cv2.imshow('im', client.frame)
+                tvec = tracker.getCameraPosition(client.frame)
+                print(
+                    "x: %.2f cm, y: %.2f cm, z: %.2f cm, Pitch is %.2f degrees, Yaw is %.2f degrees, Roll is %.2f degrees." % (
+                        tvec[0], tvec[1], tvec[2], tvec[3], tvec[4], tvec[5]))
+                ps.sendPosition(tvec[0],  tvec[1], tvec[2])
+
+            except RuntimeError:
+                print("not enough markers")
+
+            if cv2.waitKey(10) == ord('s'):
+                print(client.frame)
+                # png.from_array(client.frame, 'L').save("frame{}.png".format(i))
+                cv2.imwrite('frame{}.png'.format(i), client.frame)
+                i = i + 1
+            elif cv2.waitKey(10) == ord(' '):
+                break
     finally:
         client.close()
